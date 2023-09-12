ed7d145f66c4821127a1b8f052777c55196b2acc
vhperaltaj <victor.peralta@microside.com>
2023-09-12 14:09:32 -0600

    Updated comments and indentation

--- Proyecto/PIC18F4550/13_Motor_Driver.X/main.c
+++ Proyecto/PIC18F4550/13_Motor_Driver.X/main.c
@@ -6,127 +6,128 @@
  */
 
 // CONFIG1L
-#pragma config PLLDIV = 1       // PLL Prescaler Selection bits (No prescale (4 MHz oscillator input drives PLL directly))
-#pragma config CPUDIV = OSC1_PLL2// System Clock Postscaler Selection bits ([Primary Oscillator Src: /1][96 MHz PLL Src: /2])
-#pragma config USBDIV = 1       // USB Clock Selection bit (used in Full-Speed USB mode only; UCFG:FSEN = 1) (USB clock source comes directly from the primary oscillator block with no postscale)
+#pragma config PLLDIV = 1         // PLL Prescaler Selection bits (No prescale (4 MHz oscillator input drives PLL directly))
+#pragma config CPUDIV = OSC1_PLL2 // System Clock Postscaler Selection bits ([Primary Oscillator Src: /1][96 MHz PLL Src: /2])
+#pragma config USBDIV = 1         // USB Clock Selection bit (used in Full-Speed USB mode only; UCFG:FSEN = 1) (USB clock source comes directly from the primary oscillator block with no postscale)
 
 // CONFIG1H
-#pragma config FOSC = HSPLL_HS  // Oscillator Selection bits (HS oscillator, PLL enabled (HSPLL))
-#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
-#pragma config IESO = OFF       // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
+#pragma config FOSC = HSPLL_HS // Oscillator Selection bits (HS oscillator, PLL enabled (HSPLL))
+#pragma config FCMEN = OFF     // Fail-Safe Clock Monitor Enable bit (Fail-Safe Clock Monitor disabled)
+#pragma config IESO = OFF      // Internal/External Oscillator Switchover bit (Oscillator Switchover mode disabled)
 // CONFIG2L
-#pragma config PWRT = ON        // Power-up Timer Enable bit (PWRT enabled)
-#pragma config BOR = OFF        // Brown-out Reset Enable bits (Brown-out Reset disabled in hardware and software)
-#pragma config BORV = 3         // Brown-out Reset Voltage bits (Minimum setting 2.05V)
-#pragma config VREGEN = OFF     // USB Voltage Regulator Enable bit (USB voltage regulator disabled)
+#pragma config PWRT = ON    // Power-up Timer Enable bit (PWRT enabled)
+#pragma config BOR = OFF    // Brown-out Reset Enable bits (Brown-out Reset disabled in hardware and software)
+#pragma config BORV = 3     // Brown-out Reset Voltage bits (Minimum setting 2.05V)
+#pragma config VREGEN = OFF // USB Voltage Regulator Enable bit (USB voltage regulator disabled)
 // CONFIG2H
-#pragma config WDT = OFF        // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
-#pragma config WDTPS = 32768    // Watchdog Timer Postscale Select bits (1:32768)
+#pragma config WDT = OFF     // Watchdog Timer Enable bit (WDT disabled (control is placed on the SWDTEN bit))
+#pragma config WDTPS = 32768 // Watchdog Timer Postscale Select bits (1:32768)
 // CONFIG3H
-#pragma config CCP2MX = ON      // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
-#pragma config PBADEN = OFF     // PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
-#pragma config LPT1OSC = OFF    // Low-Power Timer 1 Oscillator Enable bit (Timer1 configured for higher power operation)
-#pragma config MCLRE = ON       // MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
+#pragma config CCP2MX = ON   // CCP2 MUX bit (CCP2 input/output is multiplexed with RC1)
+#pragma config PBADEN = OFF  // PORTB A/D Enable bit (PORTB<4:0> pins are configured as digital I/O on Reset)
+#pragma config LPT1OSC = OFF // Low-Power Timer 1 Oscillator Enable bit (Timer1 configured for higher power operation)
+#pragma config MCLRE = ON    // MCLR Pin Enable bit (MCLR pin enabled; RE3 input pin disabled)
 // CONFIG4L
-#pragma config STVREN = ON      // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
-#pragma config LVP = OFF         // Single-Supply ICSP Enable bit (Single-Supply ICSP enabled)
-#pragma config ICPRT = OFF      // Dedicated In-Circuit Debug/Programming Port (ICPORT) Enable bit (ICPORT disabled)
-#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
+#pragma config STVREN = ON // Stack Full/Underflow Reset Enable bit (Stack full/underflow will cause Reset)
+#pragma config LVP = OFF   // Single-Supply ICSP Enable bit (Single-Supply ICSP enabled)
+#pragma config ICPRT = OFF // Dedicated In-Circuit Debug/Programming Port (ICPORT) Enable bit (ICPORT disabled)
+#pragma config XINST = OFF // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
 // CONFIG5L
-#pragma config CP0 = OFF        // Code Protection bit (Block 0 (000800-001FFFh) is not code-protected)
-#pragma config CP1 = OFF        // Code Protection bit (Block 1 (002000-003FFFh) is not code-protected)
-#pragma config CP2 = OFF        // Code Protection bit (Block 2 (004000-005FFFh) is not code-protected)
-#pragma config CP3 = OFF        // Code Protection bit (Block 3 (006000-007FFFh) is not code-protected)
+#pragma config CP0 = OFF // Code Protection bit (Block 0 (000800-001FFFh) is not code-protected)
+#pragma config CP1 = OFF // Code Protection bit (Block 1 (002000-003FFFh) is not code-protected)
+#pragma config CP2 = OFF // Code Protection bit (Block 2 (004000-005FFFh) is not code-protected)
+#pragma config CP3 = OFF // Code Protection bit (Block 3 (006000-007FFFh) is not code-protected)
 // CONFIG5H
-#pragma config CPB = OFF        // Boot Block Code Protection bit (Boot block (000000-0007FFh) is not code-protected)
-#pragma config CPD = OFF        // Data EEPROM Code Protection bit (Data EEPROM is not code-protected)
+#pragma config CPB = OFF // Boot Block Code Protection bit (Boot block (000000-0007FFh) is not code-protected)
+#pragma config CPD = OFF // Data EEPROM Code Protection bit (Data EEPROM is not code-protected)
 // CONFIG6L
-#pragma config WRT0 = OFF       // Write Protection bit (Block 0 (000800-001FFFh) is not write-protected)
-#pragma config WRT1 = OFF       // Write Protection bit (Block 1 (002000-003FFFh) is not write-protected)
-#pragma config WRT2 = OFF       // Write Protection bit (Block 2 (004000-005FFFh) is not write-protected)
-#pragma config WRT3 = OFF       // Write Protection bit (Block 3 (006000-007FFFh) is not write-protected)
+#pragma config WRT0 = OFF // Write Protection bit (Block 0 (000800-001FFFh) is not write-protected)
+#pragma config WRT1 = OFF // Write Protection bit (Block 1 (002000-003FFFh) is not write-protected)
+#pragma config WRT2 = OFF // Write Protection bit (Block 2 (004000-005FFFh) is not write-protected)
+#pragma config WRT3 = OFF // Write Protection bit (Block 3 (006000-007FFFh) is not write-protected)
 // CONFIG6H
-#pragma config WRTC = OFF       // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) are not write-protected)
-#pragma config WRTB = OFF       // Boot Block Write Protection bit (Boot block (000000-0007FFh) is not write-protected)
-#pragma config WRTD = OFF       // Data EEPROM Write Protection bit (Data EEPROM is not write-protected)
+#pragma config WRTC = OFF // Configuration Register Write Protection bit (Configuration registers (300000-3000FFh) are not write-protected)
+#pragma config WRTB = OFF // Boot Block Write Protection bit (Boot block (000000-0007FFh) is not write-protected)
+#pragma config WRTD = OFF // Data EEPROM Write Protection bit (Data EEPROM is not write-protected)
 // CONFIG7L
-#pragma config EBTR0 = OFF      // Table Read Protection bit (Block 0 (000800-001FFFh) is not protected from table reads executed in other blocks)
-#pragma config EBTR1 = OFF      // Table Read Protection bit (Block 1 (002000-003FFFh) is not protected from table reads executed in other blocks)
-#pragma config EBTR2 = OFF      // Table Read Protection bit (Block 2 (004000-005FFFh) is not protected from table reads executed in other blocks)
-#pragma config EBTR3 = OFF      // Table Read Protection bit (Block 3 (006000-007FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR0 = OFF // Table Read Protection bit (Block 0 (000800-001FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR1 = OFF // Table Read Protection bit (Block 1 (002000-003FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR2 = OFF // Table Read Protection bit (Block 2 (004000-005FFFh) is not protected from table reads executed in other blocks)
+#pragma config EBTR3 = OFF // Table Read Protection bit (Block 3 (006000-007FFFh) is not protected from table reads executed in other blocks)
 // CONFIG7H
-#pragma config EBTRB = OFF      // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) is not protected from table reads executed in other blocks)
+#pragma config EBTRB = OFF // Boot Block Table Read Protection bit (Boot block (000000-0007FFh) is not protected from table reads executed in other blocks)
 // #pragma config statements should precede project file includes.
 // Use project enums instead of #define for ON and OFF.
 
 #define _XTAL_FREQ 48000000
-#include <xc.h>
-#include <stdint.h>
-#include <stdint.h>
-#include <stdbool.h>
 #include <pic18f4550.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <xc.h>
 
-uint16_t VELOCIDAD = 0; //Variable para ajustar el valor del PWM1 en CCP1
-int SUBE = 1; //Variable para control de ascenso y descenso del valor del PWM
+uint16_t VELOCIDAD = 0;       // Variable para ajustar el valor del PWM1 en CCP1
+int SUBE = 1;                 // Variable para control de ascenso y descenso del valor del PWM
 
-void InitPorts(void);
-void PWMinit(void);
-void PWM2_LoadDutyValue(uint16_t dutyValue);
-void SetPWM(void);
+void InitPorts( void );
+void PWMinit( void );
+void PWM2_LoadDutyValue( uint16_t dutyValue );
+void SetPWM( void );
 
-void main(void) {
+void main( void )
+{
     InitPorts();
     PWMinit();
-    while (1) {
+    while ( 1 ) {
         LATCbits.LATC0 = 1;
         LATCbits.LATC1 = 0;
         SUBE = 1;
-        while (VELOCIDAD < 1020 && SUBE) {
-            VELOCIDAD += 5; //Se incrementa en pasos de 15 el ciclo de trabajo
-            PWM2_LoadDutyValue(VELOCIDAD);
-            __delay_ms(40); //Cada 40 mili segundo
+        while ( VELOCIDAD < 1020 && SUBE ) {
+            VELOCIDAD += 5;   // Se incrementa en pasos de 15 el ciclo de trabajo
+            PWM2_LoadDutyValue( VELOCIDAD );
+            __delay_ms( 40 ); // Cada 40 mili segundo
         }
 
         SUBE = 0;
 
-        while (VELOCIDAD > 5 && !SUBE) {
-            VELOCIDAD -= 5; //Se incrementa en pasos de 15 el ciclo de trabajo
-            PWM2_LoadDutyValue(VELOCIDAD);
-            __delay_ms(40); //Cada 40 mili segundo
+        while ( VELOCIDAD > 5 && !SUBE ) {
+            VELOCIDAD -= 5;   // Se incrementa en pasos de 15 el ciclo de trabajo
+            PWM2_LoadDutyValue( VELOCIDAD );
+            __delay_ms( 40 ); // Cada 40 mili segundo
         }
 
         SUBE = 1;
         LATCbits.LATC0 = 0;
         LATCbits.LATC1 = 0;
 
-        __delay_ms(1000);
+        __delay_ms( 1000 );
 
         LATCbits.LATC0 = 0;
         LATCbits.LATC1 = 1;
         SUBE = 1;
-        while (VELOCIDAD < 1020 && SUBE) {
-            VELOCIDAD += 5; //Se incrementa en pasos de 15 el ciclo de trabajo
-            PWM2_LoadDutyValue(VELOCIDAD);
-            __delay_ms(40); //Cada 40 mili segundo
+        while ( VELOCIDAD < 1020 && SUBE ) {
+            VELOCIDAD += 5;   // Se incrementa en pasos de 15 el ciclo de trabajo
+            PWM2_LoadDutyValue( VELOCIDAD );
+            __delay_ms( 40 ); // Cada 40 mili segundo
         }
 
         SUBE = 0;
 
-        while (VELOCIDAD > 5 && !SUBE) {
-            VELOCIDAD -= 5; //Se incrementa en pasos de 15 el ciclo de trabajo
-            PWM2_LoadDutyValue(VELOCIDAD);
-            __delay_ms(40); //Cada 40 mili segundo
+        while ( VELOCIDAD > 5 && !SUBE ) {
+            VELOCIDAD -= 5;   // Se incrementa en pasos de 15 el ciclo de trabajo
+            PWM2_LoadDutyValue( VELOCIDAD );
+            __delay_ms( 40 ); // Cada 40 mili segundo
         }
 
         SUBE = 1;
         LATCbits.LATC0 = 0;
         LATCbits.LATC1 = 0;
-        __delay_ms(1000);
+        __delay_ms( 1000 );
     }
     return;
 }
 
-void InitPorts(void) {
+void InitPorts( void )
+{
     ADCON1bits.PCFG = 0xF;
     LATC = 0;
     TRISCbits.RC2 = 0;
@@ -134,7 +135,8 @@ void InitPorts(void) {
     TRISCbits.RC0 = 0;
 }
 
-void PWMinit(void) {
+void PWMinit( void )
+{
     PR2 = 0xFE;
     TMR2 = 0x00;
     T2CON = 0x06;
@@ -142,7 +144,8 @@ void PWMinit(void) {
     T2CONbits.TMR2ON = 1;
 }
 
-void PWM2_LoadDutyValue(uint16_t dutyValue) {
-    CCPR1L = ((dutyValue & 0x03FC) >> 2);
-    CCP1CON = ((uint8_t) (CCP1CON & 0xCF) | ((dutyValue & 0x0003) << 4));
+void PWM2_LoadDutyValue( uint16_t dutyValue )
+{
+    CCPR1L = ( ( dutyValue & 0x03FC ) >> 2 );
+    CCP1CON = ( (uint8_t)( CCP1CON & 0xCF ) | ( ( dutyValue & 0x0003 ) << 4 ) );
 }
--- Proyecto/PIC18F45K50/13_MOTOR_DRIVE.X/main.c
+++ Proyecto/PIC18F45K50/13_MOTOR_DRIVE.X/main.c
@@ -19,104 +19,86 @@
  */
 
 /*
-    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
-    
-    Subject to your compliance with these terms, you may use Microchip software and any 
-    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
-    license terms applicable to your use of third party software (including open source software) that 
+    (c) 2018 Microchip Technology Inc. and its subsidiaries.
+
+    Subject to your compliance with these terms, you may use Microchip software and any
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party
+    license terms applicable to your use of third party software (including open source software) that
     may accompany Microchip software.
-    
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
-    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS
     FOR A PARTICULAR PURPOSE.
-    
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
-    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
-    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
-    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
-    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS
     SOFTWARE.
  */
 
 #include "mcc_generated_files/mcc.h"
 
-int SUBE = 1; //Variable para control de ascenso y descenso del valor del PWM
+int SUBE = 1;                                 // Variable para control de ascenso y descenso del valor del PWM
 uint16_t VELOCIDAD = 0;
 
-void main(void) {
-    // Initialize the device
-    SYSTEM_Initialize();
+void main( void )
+{
+    SYSTEM_Initialize();                      // Inicializa el sistema
 
-    // If using interrupts in PIC18 High/Low Priority Mode you need to enable the Global High and Low Interrupts
-    // If using interrupts in PIC Mid-Range Compatibility Mode you need to enable the Global and Peripheral Interrupts
-    // Use the following macros to:
-
-    // Enable the Global Interrupts
-    //INTERRUPT_GlobalInterruptEnable();
-
-    // Disable the Global Interrupts
-    //INTERRUPT_GlobalInterruptDisable();
-
-    // Enable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptEnable();
-
-    // Disable the Peripheral Interrupts
-    //INTERRUPT_PeripheralInterruptDisable();
-
-    while (1) {
+    while ( 1 ) {
         IN_1_SetHigh();
         IN_2_SetLow();
 
         SUBE = 1;
 
-        while (VELOCIDAD < 1020 && SUBE) {
-
-            VELOCIDAD += 5; //Se incrementa en pasos de 15 el ciclo de trabajo
-            EPWM1_LoadDutyValue(VELOCIDAD); //Salida Pin_C2
-            __delay_ms(40); //Cada 40 mili segundo
+        while ( VELOCIDAD < 1020 && SUBE ) {
+            VELOCIDAD += 5;                   // Se incrementa en pasos de 15 el ciclo de trabajo
+            EPWM1_LoadDutyValue( VELOCIDAD ); // Salida Pin_C2
+            __delay_ms( 40 );                 // Cada 40 mili segundo
         }
 
         SUBE = 0;
 
-        while (VELOCIDAD > 5 && !SUBE) {
-            VELOCIDAD -= 5; //Se reduce en pasos de 15 el ciclo de trabajo
-            EPWM1_LoadDutyValue(VELOCIDAD); //Salida Pin_C2
-            __delay_ms(40);
+        while ( VELOCIDAD > 5 && !SUBE ) {
+            VELOCIDAD -= 5;                   // Se reduce en pasos de 15 el ciclo de trabajo
+            EPWM1_LoadDutyValue( VELOCIDAD ); // Salida Pin_C2
+            __delay_ms( 40 );
         }
 
         SUBE = 1;
-        
+
         IN_1_SetLow();
         IN_2_SetLow();
-        
-        __delay_ms(1000);
-        
-        
+
+        __delay_ms( 1000 );
+
         IN_1_SetLow();
         IN_2_SetHigh();
 
         SUBE = 1;
 
-        while (VELOCIDAD < 1020 && SUBE) {
+        while ( VELOCIDAD < 1020 && SUBE ) {
 
-            VELOCIDAD += 5; //Se incrementa en pasos de 15 el ciclo de trabajo
-            EPWM1_LoadDutyValue(VELOCIDAD); //Salida Pin_C2
-            __delay_ms(40); //Cada 40 mili segundo
+            VELOCIDAD += 5;                   // Se incrementa en pasos de 15 el ciclo de trabajo
+            EPWM1_LoadDutyValue( VELOCIDAD ); // Salida Pin_C2
+            __delay_ms( 40 );                 // Cada 40 mili segundo
         }
 
         SUBE = 0;
 
-        while (VELOCIDAD > 5 && !SUBE) {
-            VELOCIDAD -= 5; //Se reduce en pasos de 15 el ciclo de trabajo
-            EPWM1_LoadDutyValue(VELOCIDAD); //Salida Pin_C2
-            __delay_ms(40);
+        while ( VELOCIDAD > 5 && !SUBE ) {
+            VELOCIDAD -= 5;                   // Se reduce en pasos de 15 el ciclo de trabajo
+            EPWM1_LoadDutyValue( VELOCIDAD ); // Salida Pin_C2
+            __delay_ms( 40 );
         }
 
         SUBE = 1;
-        
+
         IN_1_SetLow();
         IN_2_SetLow();
     }
